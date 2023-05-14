daf54318d96156d07e365d1d9a43a7e1197f2009
Tamiru Taye <tamirulalan@gmail.com>
2023-05-14 18:18:34 +0000

    pkey

---
+++ problem1.sql
@@ -0,0 +1,5 @@
+   create table song(
+        id integer auto_increment primary key,
+        title varchar(100) ,
+        artist varchar(100) 
+    );
--- src/main/java/CreateTableWithPK.java
+++ src/main/java/CreateTableWithPK.java
@@ -42,6 +42,7 @@ import java.sql.Statement;
  */
 public class CreateTableWithPK {
 
+ 
     /**
      *      song table
      *      |   id  |      title        |        artist         |
